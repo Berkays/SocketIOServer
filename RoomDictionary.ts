e4b3289ab145a97467fcb48fd711eb2e32c988c1
Berkay GURSOY <berkaygursoy@gmail.com>
2018-10-21 18:56:20 +0300

    Rename RoomDictionary to RoomMap

--- RoomDictionary.ts
+++
@@ -1,6 +0,0 @@
-import { Room } from "./Room";
-
-export class RoomDictionary
-{
-    [key:string]: Room;
-}
---
+++ RoomMap.ts
@@ -0,0 +1,6 @@
+import { Room } from "./Room"
+
+export interface RoomMap
+{
+    [key:string]: Room;
+}
