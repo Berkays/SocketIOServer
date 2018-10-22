ee6296062b3dffb4f0cb5e0a3bc7864610084f95
Berkay GURSOY <berkaygursoy@gmail.com>
2018-10-22 21:24:27 +0300

    remove onclientmessage

--- RoomState.ts
+++ RoomState.ts
@@ -3,18 +3,17 @@ import { Client } from "./Client";
 
 export abstract class RoomState {
 
-    private stateEventEmitter: EventEmitter;
+    protected stateEventEmitter: EventEmitter;
 
     constructor(eventEmitter: EventEmitter) {
         this.stateEventEmitter = eventEmitter;
         this.setStateEvents();
     }
 
-    private setStateEvents(): void {
+    protected setStateEvents(): void {
         //Arrow functions to bind class instance to this reference instead of event emitter
         this.stateEventEmitter.on("onClientJoin", (client: Client) => { this.onClientJoin(client); });
         this.stateEventEmitter.on("onClientLeave", (client: Client) => { this.onClientLeave(client) });
-        this.stateEventEmitter.on("onClientwMessage", (data: string) => { this.onClientMessage(data) });
     }
 
     abstract requestJoin(client: Client): boolean;
@@ -22,8 +21,6 @@ export abstract class RoomState {
     abstract onClientJoin(client: Client): void;
 
     abstract onClientLeave(client: Client): void;
-
-    abstract onClientMessage(data: string): void
 
     abstract Serialize(): Buffer;
 }
